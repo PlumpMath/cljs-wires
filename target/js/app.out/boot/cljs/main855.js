7c6bdd3dbd487197c89f9dbc5a1238f1f7aa9e9d
freddy <freddy.teuma@smartodds.co.uk>
2016-12-16 22:30:42 +0000

    Remove target : useles...

--- target/js/app.out/boot/cljs/main855.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 1.9.293 {:static-fns true, :optimize-constants true}
-goog.provide('boot.cljs.main855');
-goog.require('cljs.core');
-goog.require('wires.app');
-wires.app.init();
