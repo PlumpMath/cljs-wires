ceb9e2636bfa2bbfb5b6a74c2613ba974eb08781
freddy <freddy.teuma@smartodds.co.uk>
2016-12-16 22:29:54 +0000

    Try to add the target to see ?

---
+++ target/js/app.out/boot/cljs/main855.js
@@ -0,0 +1,5 @@
+// Compiled by ClojureScript 1.9.293 {:static-fns true, :optimize-constants true}
+goog.provide('boot.cljs.main855');
+goog.require('cljs.core');
+goog.require('wires.app');
+wires.app.init();
